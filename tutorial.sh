8af98079f228090394e53cc44e3d832d2431153e
Piotr Wojcik <piotr_to@op.pl>
2016-11-11 14:01:17 +0100

    better catalogue hierarchy

--- tutorial.sh
+++ tutorial.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir dimensionality_reduction
-cd dimensionality_reduction
 if [ `command -v pyvenv` ]; then
 	VENV=pyvenv
 elif [ `command -v virtualenv` ]; then
@@ -10,11 +8,10 @@ elif [ `command -v venv` ]; then
 	VENV=venv
 else
 	echo "no virtualenv"
+	exit
 fi
 $VENV --system-site-packages venv
 source venv/bin/activate
-python -m pip install jupyter
+python -m pip install -r requirements.txt
 jupyter notebook
 deactivate
-cd ..
-#rm -r dimensionality_reduction
